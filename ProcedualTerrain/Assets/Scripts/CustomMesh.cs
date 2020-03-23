a5e99f629b99c972b8322a5243b50a8eded77e18
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-03-23 18:47:28 -0400

    yayyy done

--- ProcedualTerrain/Assets/Scripts/CustomMesh.cs
+++ ProcedualTerrain/Assets/Scripts/CustomMesh.cs
@@ -9,9 +9,9 @@ public class CustomMesh{
     public Vector2[] uvs;
 
     public CustomMesh(int width, int height) { //width and height of grid
-        verticies = new Vector3[(width + 1) * (height + 1)];
-        triangles = new int[width * height * 6];
-        uvs = new Vector2[(width + 1) * (height + 1)];
+        verticies = new Vector3[(width) * (height)];
+        triangles = new int[(width-1) * (height-1) * 6];
+        uvs = new Vector2[verticies.Length];
     }
 
     public Mesh CreateMesh() {
@@ -29,22 +29,29 @@ public class CustomMesh{
         int height = noiseMap.GetLength(1);
         CustomMesh customMesh = new CustomMesh(width, height);
 
-        //Generate verticies
-        for (int i = 0, v = 0; v < height; v++) {
-            for (int u = 0; u < width; i++, u++) {
-
-                customMesh.verticies[i] = new Vector3(u, noiseMap[u, v], v); //horizontal mesh, also the verticies are spaced 1 unit apart
-                customMesh.uvs[i] = new Vector2((float)(u / width), (float)(v / height));
+        //populate verticies
+        for (int v = 0, i = 0; v < height; v++) {
+            for (int u = 0; u < width; u++, i++) {
+                customMesh.verticies[i] = new Vector3(u-(width-1)/2f, noiseMap[u,v], v-(height-1)/2f);
+                customMesh.uvs[i] = new Vector2((float)u / width, (float)v / height);
             }
         }
 
         //Generate triangles
-        for (int v = 0; v < height-1; v++) {
-            for (int u = 0, ti = 0, vi = 0; u < width-1; u++, ti += 6, vi++) {
-                customMesh.triangles[ti+0] = vi;
-                customMesh.triangles[ti+2] = customMesh.triangles[ti+4] = vi+1;
-                customMesh.triangles[ti+1] = customMesh.triangles[ti+5] = vi+width;
-                customMesh.triangles[ti+3] = vi+width+1;
+        //Generate triangles
+        for (int v = 0, ti = 0, vi = 0; v < height-1; v++, vi++) {
+            for (int u = 0; u < width-1; u++, ti += 6, vi++) {
+                //triangle 1
+                customMesh.triangles[ti] = vi;
+                customMesh.triangles[ti + 1] = vi + width;
+                customMesh.triangles[ti + 2] = vi + 1;
+
+
+                //triangle 2
+                customMesh.triangles[ti + 3] = vi + 1;
+                customMesh.triangles[ti + 4] = vi + width;
+                customMesh.triangles[ti + 5] = vi + width + 1;
+
             }
         }
 
---
+++ ProcedualTerrain/Assets/Scripts/Meshy.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class Meshy : MonoBehaviour {
+
+    public int width;
+    public int height;
+
+    Vector3[] verticies;
+    int[] triangles;
+    Vector2[] uvs;
+
+    private void Start() {
+        GenerateMesh();
+    }
+
+    void GenerateMesh() {
+        verticies = new Vector3[(width+1)*(height+1)];
+        triangles = new int[width*height*6];
+        uvs = new Vector2[verticies.Length];
+
+        //populate verticies
+        for (int v = 0, i = 0; v < height+1; v++) {
+            for (int u = 0; u < width+1; u++, i++) {
+                verticies[i] = new Vector3(u, 0, v);
+                uvs[i] = new Vector2((float)u / width, (float)v / height);
+            }
+        }
+
+        //Generate triangles
+        //Generate triangles
+        for (int v = 0, ti = 0, vi = 0; v < height; v++, vi++) {
+            for (int u = 0; u < width; u++, ti += 6, vi++) {
+                //triangle 1
+                triangles[ti] = vi;
+                triangles[ti + 1] = vi + width + 1;
+                triangles[ti + 2] = vi + 1;
+
+                
+                //triangle 2
+                triangles[ti + 3] = vi + 1;
+                triangles[ti + 4] = vi + width + 1;
+                triangles[ti + 5] = vi + width + 2;
+                
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        mesh.vertices = verticies;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+    }
+
+    private void OnDrawGizmos() {
+        if (verticies == null) { return; }
+        Gizmos.color = Color.blue;
+
+        foreach(Vector3 v in verticies) {
+            if (v == null) { continue; }
+            Gizmos.DrawSphere(v,0.05f);
+        }
+    }
+
+}
