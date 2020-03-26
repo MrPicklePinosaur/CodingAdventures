d1d146a7151ab2251e59be7239158e03fcf366ec
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-03-26 19:25:45 -0400

    basic input

---
+++ Trampoline/Assets/Scripts/MeshDeformer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour {
+
+    Vector3[] orgVertices; //position of the original vertices
+    Vector3[] dspVertices; //current position of vertices
+    Vector3[] velVertices; //velocities of each vertice
+
+    public void InitMeshDeformer() {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        orgVertices = mesh.vertices;
+        dspVertices = new Vector3[orgVertices.Length];
+        for (int i = 0; i < orgVertices.Length; i++) {
+            dspVertices[i] = orgVertices[i];
+        }
+    }
+
+    public void ApplyForce(float force, Vector3 point) {
+
+    }
+}
---
+++ Trampoline/Assets/Scripts/Pointer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour {
+
+    public float force;
+    public float forcePointOffset; //the distance from the surface of the mesh where the force is applied
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            FindPoint();
+        }
+    }
+
+    public void FindPoint() { //raycast onto mesh
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(camRay, out hit)) { //if the ray hit anything
+
+            MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+            if (deformer) { //if not null
+                Vector3 forcepoint = hit.point + hit.normal*forcePointOffset;
+                Debug.DrawLine(Camera.main.transform.position,forcepoint);
+
+                
+            }
+
+            
+        }
+    }
+}
