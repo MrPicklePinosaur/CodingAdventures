920dbc9f86beeba5dc3d5b6a724a560996ac5c85
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-04-15 15:38:50 -0400

    see to other side of portal

--- Portals/Assets/Scripts/Portal.cs
+++ Portals/Assets/Scripts/Portal.cs
@@ -2,35 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
 public class Portal : MonoBehaviour {
 
     public GameObject linkedPortal;
     Camera portalCam; //used to scout the output
     Camera viewerCam;
+    Renderer screen;
 
     RenderTexture rt;
 
     void Start() {
         portalCam = GetComponentInChildren<Camera>();
         viewerCam = Camera.main;
+        screen = GetComponentInChildren<Renderer>();
 
         rt = new RenderTexture(Screen.width, Screen.height,0);
         portalCam.targetTexture = rt;
+        portalCam.enabled = false;
 
-        Renderer r = GetComponent<Renderer>();
-        r.material.SetTexture("_MainTex",rt);
+        screen.material.SetTexture("_MainTex",rt);
 
     }
 
     void Update() {
+
         updateLinkedCamPosition();
+
     }
 
     void updateLinkedCamPosition() {
+
+        linkedPortal.GetComponent<Portal>().disableScreen();
+
         Matrix4x4 mat = linkedPortal.transform.localToWorldMatrix * transform.worldToLocalMatrix * viewerCam.transform.localToWorldMatrix;
         portalCam.transform.SetPositionAndRotation(mat.GetColumn(3),mat.rotation);
+        portalCam.Render();
+
+        linkedPortal.GetComponent<Portal>().enableScreen();
     }
 
-    
+    public void enableScreen() {
+        screen.enabled = true;
+    }
+    public void disableScreen() {
+        screen.enabled = false;
+    }
+
 }
