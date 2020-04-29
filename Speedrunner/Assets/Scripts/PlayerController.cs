ea5535b73f9f53493d6a20d86974421b088e857c
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-04-29 00:11:27 -0400

    jumping

--- Speedrunner/Assets/Scripts/PlayerController.cs
+++ Speedrunner/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
+    public float maxSpeed;
+    public float moveDamping;
+
     public Vector2 lookSpeed;
+    public float lookClamp;
+
+    public float jumpSpeed;
+    public float maxJumpHoldTime;
+    Timer jumpTimer;
+    bool isOnGround;
 
     //camera stuff
     private float pitch = 0f;
@@ -18,6 +27,11 @@ public class PlayerController : MonoBehaviour {
     void Start() {
         rb = GetComponent<Rigidbody>();
         playerCamera = GetComponentInChildren<Camera>();
+
+        jumpTimer = gameObject.AddComponent<Timer>();
+        jumpTimer.duration = maxJumpHoldTime;
+
+        Cursor.lockState = CursorLockMode.Locked; //cant move mouse
     }
 
 
@@ -25,17 +39,54 @@ public class PlayerController : MonoBehaviour {
 
         Vector2 usin = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
-        //rb.velocity = new Vector3(transform.forward.x*usin.x,0,transform.forward.z*usin.y)*Time.deltaTime;
 
-        //transform.position += new Vector3(playerCamera.transform.forward.x * usin.x, 0, playerCamera.transform.forward.z * usin.y) * Time.deltaTime;
-        transform.position += playerCamera.transform.forward * usin.y * moveSpeed * Time.deltaTime;
-        transform.position += playerCamera.transform.right * usin.x * moveSpeed * Time.deltaTime;
+        rb.velocity += Vector3.ProjectOnPlane(playerCamera.transform.forward,Vector3.up).normalized * usin.y * moveSpeed * Time.deltaTime;
+        rb.velocity += Vector3.ProjectOnPlane(playerCamera.transform.right, Vector3.up).normalized * usin.x * moveSpeed * Time.deltaTime;
+
+        //apply some friction
+        float x_damp = 0;
+        if (Mathf.Abs(rb.velocity.x) > 0) {
+            x_damp = -1 * rb.velocity.x / Mathf.Abs(rb.velocity.x) * moveDamping * Time.deltaTime;
+        }
+        float z_damp = 0;
+        if (Mathf.Abs(rb.velocity.z) > 0) {
+            z_damp = -1 * rb.velocity.z / Mathf.Abs(rb.velocity.z) * moveDamping * Time.deltaTime;
+        }
+        rb.velocity += new Vector3(x_damp,0,z_damp);
+
+
+        //max movement speed
+        rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x,-maxSpeed,maxSpeed),rb.velocity.y, Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed));
+        
+
+        //Jumping
+        if (Input.GetKey(KeyCode.Space) && !jumpTimer.isActive && isOnGround) { //if not jumping, start jump
+            jumpTimer.activate();
+            //possibly add an initial jump impulse force thingy
+        }
+        if (Input.GetKey(KeyCode.Space) && jumpTimer.isActive) { //hold to jump higher
+            rb.velocity += Vector3.up * jumpSpeed * Time.deltaTime;
+        } else { //otherwise, if jump key is released, stop jumping
+            jumpTimer.deactivate();
+        }
+        
 
         //Camera
         yaw += lookSpeed.x * Input.GetAxis("Mouse X");
         pitch -= lookSpeed.y * Input.GetAxis("Mouse Y");
 
+        pitch = Mathf.Clamp(pitch,-lookClamp,lookClamp); //clamp vertical look
+
         playerCamera.transform.eulerAngles = new Vector3(pitch, yaw, 0f);
 
+        isOnGround = false;
+
     }
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("floor")) {
+            isOnGround = true;
+        }
+    }
+
 }
---
+++ Speedrunner/Assets/Scripts/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    [HideInInspector] public float cooldown;
+    [HideInInspector] public float duration;
+
+    [HideInInspector] protected float cooldown_counter;
+    [HideInInspector] protected float duration_counter;
+
+    public bool isActive;
+
+    protected void Start() {
+        isActive = false;
+        cooldown_counter = cooldown;
+        duration_counter = duration;
+    }
+
+    protected void Update() {
+
+        if (isActive == true && duration_counter > 0) {
+            duration_counter -= Time.deltaTime;
+            passiveEffect();
+        }
+
+        if (isActive == false && cooldown_counter > 0) {
+            cooldown_counter -= Time.deltaTime;
+        }
+
+        if (duration_counter <= 0) {
+            deactivate();
+        }
+
+
+    }
+
+    public virtual void activate() {
+        if (cooldown_counter > 0 || isActive) { return; }
+        cooldown_counter = cooldown;
+        duration_counter = duration;
+        isActive = true;
+        activateEffect();
+    }
+
+    public virtual void deactivate() {
+        if (isActive == false) { return; }
+        isActive = false;
+        deactivateEffect();
+    }
+
+    public virtual void activateEffect() { }
+
+    public virtual void passiveEffect() { }
+
+    public virtual void deactivateEffect() { }
+
+}
