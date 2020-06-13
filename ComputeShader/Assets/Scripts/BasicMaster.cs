ff53e2b4f821f7e98a698da0b3eb63f6e8df8f97
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-06-13 15:21:22 -0400

    restarted

---
+++ ComputeShader/Assets/Scripts/BasicMaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMaster : MonoBehaviour {
+
+    public ComputeShader BasicShader;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+
+    }
+
+    private void CreateRenderTexture() {
+        
+    }
+
+   
+    
+}
